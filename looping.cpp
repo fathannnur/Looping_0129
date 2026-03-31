d0ec6289d592333737dce2dd49c1e1bdb7915d87
fathannnur <carelius02@gmail.com>
2026-03-31 19:48:58 +0700

    Menambahkan string

--- looping.cpp
+++ looping.cpp
@@ -39,7 +39,20 @@ int main(){
     cout<<endl;
 }
 
-Logika not
+ Logika not
 #include <iostream>
 using namespace std;
 
+int main (){
+    float nilB,nilM,rerata;
+    string status;
+
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
+
+    rerata = (nilB+nilM)/2;
+
+    
+}
